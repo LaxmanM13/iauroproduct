5065164998ee36941480ed95af0be095e860cb27
Laxman <laxmanmule13@gmail.com>
2022-06-28 15:24:14 +0530

    pkg chnages

--- src/main/java/com/iauro/iauroproductassignment/entities/Product.java
+++ src/main/java/com/iauro/iauroproductassignment/entities/Product.java
@@ -1,6 +1,7 @@
 package com.iauro.iauroproductassignment.entities;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
